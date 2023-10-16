554da684a40df296e952b0bd7dd54bb8925428f6
PakaOxO <arpeggiogio@gmail.com>
2023-10-16 16:03:49 +0900

    update: 231016 스터디 문제

--- Algorithm/2023.10/PakaOxO/미로탈출명령어.js
+++ Algorithm/2023.10/PakaOxO/미로탈출명령어.js
@@ -15,40 +15,36 @@ const solution = (n, m, x, y, r, c, k) => {
 
   /* 메인 로직 */
   x--, y--, r--, c--; // 입력값에서 -1씩
-  bfs(x, y, 0, ""); // 시작점부터 bfs 탐색
+  dfs(x, y, 0, ""); // 시작점부터 bfs 탐색
 
   /* 정답 반환 */
   return answer || "impossible";
 
   /**
    * dfs
+   * **bfs는 k보다 작은 모든 경로를 우선 탐색하기 때문에 시간초과
+   * **heap을 사용해 풀거나 하는 방식으로 bfs 사용 가능
    */
-  function bfs(sr, sc) {
-    const queue = [[sr, sc, 0, ""]];
-    const MAX = 2500; // 최대 이동가능 거리는 k의 최대값(~2500)
-    const dists = Array.from({ length: n }, () =>
-      Array.from({ length: m }, () => Array.from({ length: MAX + 1 }, () => false))
-    );
+  function dfs(cr, cc, dist, path) {
+    let d = Math.abs(cr - r) + Math.abs(cc - c);
+    if (d % 2 !== (k - dist) % 2) return; // 목표 이동과 실제 이동 홀짝이 다르면 return
+    if (answer !== null) return;
 
-    while (queue.length > 0) {
-      const [cr, cc, dist, path] = queue.shift();
-      if (dist === k) {
-        // 알파벳 순으로 탐색했으므로 처음 도착한 경우가 정답
-        if (cr === r && cc === c) {
-          answer = path;
-          break;
-        }
-        continue;
+    if (dist === k) {
+      if (cr === r && cc === c) {
+        answer = path;
+        return;
       }
+      return;
+    }
 
-      for (let i = 0; i < drc.length; i++) {
-        // 다음 위치
-        const [nr, nc] = [cr + drc[i][0], cc + drc[i][1]];
-        if (nr < 0 || nc < 0 || nr >= n || nc >= m) continue; // 맵 밖이면 X
-        if (dists[nr][nc][dist]) continue; // 동일한 거리로 이미 방문했으면 X
-        dists[nr][nc][dist] = true;
-        queue.push([nr, nc, dist + 1, path + dir[i]]);
-      }
+    for (let i = 0; i < drc.length; i++) {
+      // 다음 위치
+      const [nr, nc] = [cr + drc[i][0], cc + drc[i][1]];
+      if (nr < 0 || nc < 0 || nr >= n || nc >= m) continue; // 맵 밖이면 X
+      d = Math.abs(nr - r) + Math.abs(nc - c);
+      if (d > k - dist - 1) continue;
+      dfs(nr, nc, dist + 1, path + dir[i]);
     }
   }
 };
