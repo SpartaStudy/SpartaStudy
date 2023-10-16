ffb6dc872173e6df5b899ba75220fb0327f9cf13
graywhales <109879750+graywhales@users.noreply.github.com>
2023-10-16 16:19:39 +0900

    Update 미로탈출명령어.java

--- Algorithm/2023.10/graywhales/미로탈출명령어.java
+++ Algorithm/2023.10/graywhales/미로탈출명령어.java
@@ -1,94 +1,77 @@
+import java.util.*;
+/*
+    bfs사용 사전순으로 정렬 위해 우선순위큐 사용
+    + 가지치기를 위한 이동가능 여부 판별(impossible,도착가능한지, 범위 벗어나는지)
+*/
+
 class Solution {
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        // d l r u
-        // 움직이는걸 먼저 할건데 d l r u 순으로... 어라 이러면 안되겠네
-        String answer = "";
-        int dx=Math.abs(r-x);
-        int dy=Math.abs(c-y);
-        //딱떨어질때
-        if(dx+dy==k) {
-            //도착지점이 아래쪽일경우
-            if(r-x>0){
-                for(int i=0;i<dx;i++){
-                    answer+="d";
-                }
-            }
-            //도착지점이 왼쪽일경우
-            if(c-y<0){
-                for(int i=0;i<dy;i++){
-                    answer+="l";
-                }
-            }
-            //도착지점이 오른쪽일경우
-            if(c-y>0){
-                for(int i=0;i<dy;i++){
-                    answer+="r";
-                }
-            }
-            //도착지점이 윗쪽일경우
-            if(r-x<0){
-                for(int i=0;i<dx;i++){
-                    answer+="u";
-                }
-            }
-        //떨어지는거 + 짝수배 (헛걸음 왕복)
-        }else if(dx+dy<k&&(k-dx-dy)%2==0){
-            int diff=(k-dx-dy)/2;
-            int nx=x;
-            int ny=y;
-            int u=0;
-            int l=0;
-            if(r-x>0){
-                for(int i=0;i<dx;i++){
-                    answer+="d";
-                }
-            }
-            //아랫쪽 헛걸음
-            if(n>r){
-                while(diff-->0){
-                    answer+="d";
-                    answer+="u";
-                }
-            }
-            if(c-y<0){
-                for(int i=0;i<dy;i++){
-                    answer+="l";
-                }
-            }
-            //왼쪽 헛걸음
-            if(c!=1){
-                while(diff-->0){
-                    answer+="l";
-                    answer+="r";
-                }
-            }
-            if(c-y>0){
-                for(int i=0;i<dy;i++){
-                    answer+="r";
-                }
-            }
-            //오른쪽 헛걸음
-            if(c!=m){
-                while(diff-->0){
-                    answer+="r";
-                    answer+="l";
-                }
-            }
-            if(r-x<0){
-                for(int i=0;i<dx;i++){
-                    answer+="u";
-                }
-            }
-            //윗쪽 헛걸음
-            if(r>1){
-                while(diff-->0){
-                    answer+="u";
-                    answer+="d";
-                }
-            }
-        }else{
-            answer="impossible";
+    
+    static class Node implements Comparable<Node>{
+        String str;
+        int x,y,cnt;
+        
+        public Node(String str,int x,int y,int cnt){
+            this.str=str;
+            this.x=x;
+            this.y=y;
+            this.cnt=cnt;
         }
+        
+        @Override
+        public int compareTo(Node o) {
+            return this.str.compareTo(o.str);
+        }
+    }
+    
+    static int n,m,r,c,k;
+    static String answer;
+    static int[] dx={1,0,0,-1};
+    static int[] dy={0,1,-1,0};
+    static String[] ds={"d","r","l","u"};
+    
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        this.n = n;
+        this.m = m;
+        this.r = r;
+        this.c = c;
+        this.k = k;
+        
+        answer = "impossible";
+        
+        int dist=Math.abs(x-r)+Math.abs(y-c);
+        
+        if(k-dist<0||(k-dist)%2!=0){//불가능 조건
+            answer="impossible";
+        }else{
+            answer=bfs(x,y);
+        }
+        
         return answer;
+    }
+    
+    private static String bfs(int sx,int sy){
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        pq.offer(new Node("",sx,sy,0));
+        int x,y,cnt,nx,ny;
+        String str;
+        while(!pq.isEmpty()){
+            Node node=pq.poll();
+            x=node.x;
+            y=node.y;
+            cnt=node.cnt;
+            str=node.str;
+            if(cnt==k&&x==r&&y==c){
+                return node.str;
+            }
+            //도착지까지 이동불가
+            if(Math.abs(r-x)+Math.abs(c-y)>k-cnt) continue;
+            for(int i=0;i<4;i++){
+                nx=x+dx[i];
+                ny=y+dy[i];
+                if(nx<1||ny<1||nx>n||ny>m||cnt>k) continue;
+                pq.offer(new Node(str+ds[i],nx,ny,cnt+1));
+            }
+        }
+        return "x";
     }
 }
