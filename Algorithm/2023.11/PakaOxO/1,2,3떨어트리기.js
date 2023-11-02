b7854ca0a13f0f457e7868d8f8650bd0bb1f47a9
PakaOxO <arpeggiogio@gmail.com>
2023-11-02 13:50:19 +0900

    update: 231102 스터디 문제 풀이 수정

--- Algorithm/2023.11/PakaOxO/1,2,3떨어트리기.js
+++ Algorithm/2023.11/PakaOxO/1,2,3떨어트리기.js
@@ -3,110 +3,63 @@
  *  - 문제 분류: 트리
  */
 const solution = (edges, target) => {
-  class Node {
-    constructor(value) {
-      this.value = value;
-      this.sum = 0;
-      this.children = [];
-      this.nextChildIdx = -1;
-    }
-  }
-
   /* 변수 관리 */
   const N = edges.length;
-  const nodes = Array.from({ length: N + 1 }, (_, idx) => new Node(idx));
-  const INF = Number.MAX_SAFE_INTEGER;
-  let maxDepth = INF;
-  const comb = [];
+  const count = Array.from({ length: N + 1 }, () => 0);
+  const children = Array.from({ length: N + 1 }, () => []);
+  let visit = [];
   let answer = [];
 
   /* 메인 로직 */
   for (let i=0; i<edges.length; i++) {
     const [parent, child] = edges[i];
     // 노드에 자식노드 정보 추가
-    nodes[parent - 1].children.push(nodes[child - 1]);
+    children[parent - 1].push(child - 1);
+  }
+  for (let i=0; i<=N; i++) children[i].sort((a, b) => a - b);
+
+  while (true) {
+    const flag = check(); // 리프노드 방문개수 유효성 체크
+    if (flag < 0) { // target이 0인데 방문 되었을 경우
+      visit = null;
+      break;
+    }
+    if (flag > 0) break; // 조건에 맞는 최소 방문 횟수를 찾음
+
+    let leaf = 0; // 방문 리프노드를 찾아보자
+    while (children[leaf].length > 0) {
+      const len = children[leaf].length;
+      leaf = children[leaf][count[leaf]++ % len];
+    }
+    count[leaf]++;
+    visit.push(leaf);
   }
 
-  for (let i=0; i<N; i++) {
-    if (nodes[i].children.length > 0) {
-      // 번호가 작은 순서대로 자식 노드 정렬
-      nodes[i].children.sort((a, b) => a.value - b.value);
-      nodes[i].nextChildIdx = 0;
+  let pointer = 0; // 리프노드에 삽입할 숫자를 정해보자
+  while (visit !== null && pointer < visit.length) {
+    const leaf = visit[pointer++];
+    for (let i=1; i<=3; i++) {
+      if ((target[leaf] - i) > (count[leaf] - 1) * 3) continue; // 이 숫자를 넣으면 target 못 채움
+      answer.push(i);
+      target[leaf] -= i;
+      count[leaf]--;
+      break;
     }
   }
-  dfs(0);
 
-
+  if (answer.length === 0) answer.push(-1);
   /* 정답 반환 */
   return answer;
 
-  // dfs
-  function dfs(depth) {
-    if (depth > maxDepth) return;
-    if (check()) {
-      maxDepth = depth;
-      answer.push([...comb]);
-      return;
-    }
-
-    for (let i=3; i>=1; i--) {
-      const flag = push(i);
-
-      if (flag) {
-        comb.push(i);
-        dfs(depth + 1);
-        comb.pop();
-        pop(i);
-      }
-    }
-  }
-
-  // push
-  function push(num) {
-    let currNode = nodes[0];
-    while (currNode.children.length > 0) {
-      const currChild = currNode.nextChildIdx;
-      currNode = currNode.children[currChild];
-    }
-
-    if (currNode.sum + num > target[currNode.value]) return false;
-    
-    currNode = nodes[0];
-    while (currNode.children.length > 0) {
-      const childrenLen = currNode.children.length;
-      const currChild = currNode.nextChildIdx;
-      const nextChild = (currNode.nextChildIdx + 1) % childrenLen;
-      
-      currNode.nextChildIdx = nextChild;
-      currNode = currNode.children[currChild];
-    }
-    currNode.sum += num;
-
-    return true;
-  }
-
-  // pop
-  function pop(num) {
-    let currNode = nodes[0];
-    while (currNode.children.length > 0) {
-      const len = currNode.children.length;
-      const prevChild = (currNode.nextChildIdx - 1 + len) % len;
-
-      currNode.nextChildIdx = prevChild;
-      currNode = currNode.children[prevChild];
-    }
-
-    currNode.sum -= num;
-  }
-
   // check
   function check() {
-    const sums = [];
     for (let i=0; i<=N; i++) {
-      if (nodes[i].sum !== target[i]) return false;
-      sums.push(nodes[i].sum);
+      if (children[i].length > 0) continue;
+      if (target[i] === 0 && count[i] > 0) return -1;
+      if (target[i] < count[i]) return -1;
+      if (!(target[i] >= count[i] && target[i] <= count[i] * 3)) return 0;
     }
-    return true;
+    return 1;
   }
 }
 
