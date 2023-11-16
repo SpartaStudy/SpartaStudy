e55adf424997456991cb913ad31e3ae5df4f8998
Kwon <power916@daum.net>
2023-11-16 11:48:51 +0900

    231116 wolfy916 algorithm 카드 짝 맞추기

--- Algorithm/2023.11/wolfy916/코딩테스트 기출문제/카드 짝 맞추기.py
+++ Algorithm/2023.11/wolfy916/코딩테스트 기출문제/카드 짝 맞추기.py
@@ -24,8 +24,13 @@ def bfs(board, N, M, si, sj, ei, ej, log):
     found_cards_cnt = []
     while q:
         vi, vj = q.popleft()
+        # 카드 짝이 필요한 경우
+        # -> 해당 짝을 찾을때까지 반복
         if len(log) % 2:
-            if (vi, vj) == (ei, ej): return visited[vi][vj] + 1
+            if (vi, vj) == (ei, ej):
+                return visited[vi][vj] + 1
+        # 새로운 카드가 필요한 경우
+        # -> si, sj에서 최소 조작으로 갈 수 있는 카드를 모두 탐색
         else:
             if board[vi][vj] and (vi, vj) not in log:
                 for f_card in found_cards:
@@ -36,11 +41,13 @@ def bfs(board, N, M, si, sj, ei, ej, log):
                     found_cards.append((vi, vj))
                     found_cards_cnt.append(visited[vi][vj] + 1)
         for di, dj in delta:
+            # 방향키
             ni, nj = vi + di, vj + dj
             if ni < 0 or nj < 0 or ni >= N or nj >= M: continue
             if visited[vi][vj] + 1 < visited[ni][nj]:
                 visited[ni][nj] = visited[vi][vj] + 1
                 q.append((ni, nj))
+            # ctrl + 방향키
             ti, tj = ni, nj
             while True:
                 if ni < 0 or nj < 0 or ni >= N or nj >= M:
@@ -57,10 +64,13 @@ def bfs(board, N, M, si, sj, ei, ej, log):
 # [C] dfs 탐색 함수
 def dfs(board, vi, vj, N, M, cards, log, cnt):
     global answer
-    if cnt > answer: return
+    # 조작횟수가 갱신된 answer이상이면 탐색할 필요 없음
+    if cnt >= answer: return
+    # answer값 최소값으로 갱신
     if len(cards) == len(log):
         answer = min(answer, cnt)
         return
+    # 카드 짝이 필요할 때
     if len(log) % 2:
         for ei, ej in cards:
             if (vi, vj) == (ei, ej): continue
@@ -72,6 +82,7 @@ def dfs(board, vi, vj, N, M, cards, log, cnt):
             cards, log + [(ei, ej)],
             cnt + add
         )
+    # 새로운 카드가 필요할 때
     else:
         f_cards, f_cards_cnt = bfs(board, N, M, vi, vj, 0, 0, log)
         for card, add in zip(f_cards, f_cards_cnt):
