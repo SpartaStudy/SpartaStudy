c3b59bdcdc1d6d09b575e33ecd10b6a8eba01ffd
PakaOxO <arpeggiogio@gmail.com>
2023-11-20 16:01:23 +0900

    update: 231120, 스터디 문제 풀이 수정

--- Algorithm/2023.11/PakaOxO/양과늑대.js
+++ Algorithm/2023.11/PakaOxO/양과늑대.js
@@ -1,6 +1,6 @@
 /**
- * Programmers_양과 특대
- *  - 문제 분류: 트리
+ * Programmers_양과 늑대
+ *  - 문제 분류: 트리, 그래프 탐색
  */
 const solution = (info, edges) => {
   // 이진트리 노드 클래스
@@ -11,6 +11,7 @@ const solution = (info, edges) => {
       this.left = null;
       this.right = null;
       this.parent = null;
+      this.path = [];
     }
 
     setChild(child) {
@@ -25,13 +26,22 @@ const solution = (info, edges) => {
     setParent(parent) {
       this.parent = parent;
     }
+
+    hasPath(path) {
+      for (let p of this.path) {
+        if (p === path) return true;
+      }
+      return false;
+    }
+
+    addPath(path) {
+      this.path.push(path);
+    }
   }
 
   /* 변수 관리 */
   const N = info.length;
   const nodes = Array.from({ length: N }, (_, idx) => new Node(idx, info[idx]));
-  const visited = Array.from({ length: N }, () => false);
-  const dp = Array.from({ length: N }, () => 0);
   let answer = 0;
 
   /* 메인 로직 */
@@ -45,48 +55,45 @@ const solution = (info, edges) => {
   }
 
   // 루트노드부터 dfs
-  visited[0] = true;
-  dfs(nodes[0], 1, 0);
+  dfs(nodes[0], 0, 0, 0);
 
   /* 정답 반환 */
   return answer;
 
   // dfs
-  function dfs(currNode, sCount, wCount) {
-    if (dp[currNode.idx] >= sCount || sCount <= wCount) return;
-    dp[currNode.idx] = sCount;
-    answer = Math.max(answer, sCount);
-    if (currNode.idx === 0) {
-      console.log(visited);
+  function dfs(currNode, cnt0, cnt1, path) {
+    // 현재 인덱스
+    const idx = currNode.idx;
+
+    // cnt 증가
+    if (currNode.val === 0) {
+      if ((path & (1 << idx)) === 0) cnt0++;
+    } else {
+      if ((path & (1 << idx)) === 0) cnt1++;
+    }
+    if (cnt0 <= cnt1) return;
+
+    answer = Math.max(answer, cnt0);
+
+    // 현재 위치까지의 경로 갱신
+    const newPath = path | (1 << idx);
+    if (currNode.hasPath(newPath)) return;
+    currNode.addPath(newPath);
+
+    if (currNode.left !== null) {
+      dfs(currNode.left, cnt0, cnt1, newPath);
     }
 
-    const [left, right, parent] = [currNode.left, currNode.right, currNode.parent];
-    let [sDiff, wDiff] = [0, 0];
-
-    if (left !== null) {
-      if (!visited[left.idx]) {
-        [sDiff, wDiff] = [left.val === 0 ? 1 : 0, left.val === 1 ? 1 : 0];
-      }
-      visited[left.idx] = true;
-      dfs(left, sCount + sDiff, wCount + wDiff);
-      visited[left.idx] = false;
+    if (currNode.right !== null) {
+      dfs(currNode.right, cnt0, cnt1, newPath);
     }
 
-    if (right !== null) {
-      if (!visited[right.idx]) {
-        [sDiff, wDiff] = [right.val === 0 ? 1 : 0, right.val === 1 ? 1 : 0];
-      }
-      visited[right.idx] = true;
-      dfs(right, sCount + sDiff, wCount + wDiff);
-      visited[right.idx] = false;
-    }
-
-    if (parent !== null) {
-      dfs(parent, sCount, wCount);
+    if (currNode.parent !== null) {
+      dfs(currNode.parent, cnt0, cnt1, newPath);
     }
   }
 }
 
 console.log(solution([0,0,1,1,1,0,1,0,1,0,1,1],	[[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]));
-// console.log(solution([0,1,0,1,1,0,1,0,0,1,0],	[[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]));
-// console.log(solution([0, 1], [[0, 1]]));
+console.log(solution([0,1,0,1,1,0,1,0,0,1,0],	[[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]));
+console.log(solution([0, 1], [[0, 1]]));
