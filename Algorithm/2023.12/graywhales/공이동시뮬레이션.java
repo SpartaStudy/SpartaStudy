9fa0d33b6e97e8dfd50d8daff0054ded08d4cedd
graywhales <109879750+graywhales@users.noreply.github.com>
2023-12-05 11:42:16 +0900

    refactor: 메서드분리

--- Algorithm/2023.12/graywhales/공이동시뮬레이션.java
+++ Algorithm/2023.12/graywhales/공이동시뮬레이션.java
@@ -1,22 +1,114 @@
 /*
 구현
-상하랑 좌우가 독립적인 계란말이라 생각하자
+상하랑 좌우가 독립적인
+계란말이라 생각하자
 */
+
+    
 class Solution {
-    long ans=0;
     
-    int rf=1;//0행 뭉침
-    int re=1;//n-1행 뭉침
-    int cf=1;//0열 뭉침
-    int ce=1;//m-1열 뭉침
-    
-    int r=0;//가장 앞행 position
-    int c=0;//가장 앞열 position
-    
+    int rf=1;//0행 갯수
+    int re=1;//마지막행 갯수
+    int cf=1;//0열 갯수
+    int ce=1;//마지막열 갯수
+
+    int rp =0;//가장 앞행 position
+    int cp =0;//가장 앞열 position
+
     int rl;//현재행 길이
     int cl;//현재열 길이
     
+    int mm;
+    int nn;
+    
+    public void moveLeft(int step){
+        if(cp -step<0){
+            cf+=Math.abs(cp -step);
+            cl-=Math.abs(cp -step);
+            if(cl<=0){
+                cl=1;
+                cf=mm;
+                ce=mm;
+            }
+            cp =0;
+        }else{
+            cp -=step;
+        }
+    }
+    public void moveRight(int step){
+        if(cp +cl+step>mm){
+            ce+=(cp +cl+step-mm);
+            cl-=(cp +cl+step-mm);
+            cp +=step;
+            if(cl<=1){
+                cl=1;
+                cf=mm;
+                ce=mm;
+            }
+            if(cp >=mm){
+                cp =mm-1;
+            }
+        }else{
+            cp +=step;
+        }
+    }
+    public void moveUp(int step){
+        if(rp -step<0){
+            rf+=Math.abs(rp -step);
+            rl-=Math.abs(rp -step);
+            if(rl<=1){
+                rl=1;
+                rf=nn;
+                re=nn;
+            }
+            rp =0;
+        }else{
+            rp -=step;
+        }
+    }
+    public void moveDown(int step){
+        if(rp +rl+step>nn){
+            re+=(rp +rl+step-nn);
+            rl-=(rp +rl+step-nn);
+            rp +=step;
+            if(rl<=0){
+                rl=1;
+                rf=nn;
+                re=nn;
+            }
+            if(rp >=nn){
+                rp =nn-1;
+            }
+        }else{
+            rp +=step;
+        }
+    }
+    public long count(int x,int y){
+        long r=0;
+        long c=0;
+        if(rp <=x && x< rp +rl){
+            if(rp ==x){
+                r=rf;
+            }else if(rp +rl-1==x){
+                r=re;
+            }else{
+                r=1;
+            }
+        }
+        if(cp <=y && y< cp +cl){
+            if(cp ==y){
+                c=cf;
+            }else if(cp +cl-1==y){
+                c=ce;
+            }else{
+                c=1;
+            }
+        }
+        return r*c;
+    }
     public long solution(int n, int m, int x, int y, int[][] queries) {
+        nn=n;
+        mm=m;
         rl=n;
         cl=m;
         for (int[] query : queries) {
@@ -24,88 +116,17 @@ class Solution {
             int step=query[1];
 
             if(op==0){//좌
-                if(c-step<0){
-                    cf+=Math.abs(c-step);
-                    cl-=Math.abs(c-step);
-                    if(cl<=0){
-                        cl=1;
-                        cf=m;
-                        ce=m;
-                    }
-                    c=0;
-                }else{
-                    c-=step;
-                }
+                moveLeft(step);
             }else if(op==1){//우
-                if(c+cl+step>m){
-                    ce+=(c+cl+step-m);
-                    cl-=(c+cl+step-m);
-                    c+=step;
-                    if(cl<=1){
-                        cl=1;
-                        cf=m;
-                        ce=m;
-                    }
-                    if(c>=m){
-                        c=m-1;
-                    }
-                }else{
-                    c+=step;
-                }
+                moveRight(step);
             }else if(op==2){//상
-                if(r-step<0){
-                    rf+=Math.abs(r-step);
-                    rl-=Math.abs(r-step);
-                    if(rl<=1){
-                        rl=1;
-                        rf=n;
-                        re=n;
-                    }
-                    r=0;
-                }else{
-                    r-=step;
-                }
+                moveUp(step);
             }else if(op==3){//하
-                if(r+rl+step>n){
-                    re+=(r+rl+step-n);
-                    rl-=(r+rl+step-n);
-                    r+=step;
-                    if(rl<=0){
-                        rl=1;
-                        rf=n;
-                        re=n;
-                    }
-                    if(r>=n){
-                        r=n-1;
-                    }
-                }else{
-                    r+=step;
-                }
-            }
-            
-        }
-        long rCount=0;
-        long cCount=0;
-        if(r<=x && x<r+rl){
-            if(r==x){
-                rCount=rf;
-            }else if(r+rl-1==x){
-                rCount=re;
-            }else{
-                rCount=1;
-            }
-        }
-        if(c<=y && y<c+cl){
-            if(c==y){
-                cCount=cf;
-            }else if(c+cl-1==y){
-                cCount=ce;
-            }else{
-                cCount=1;
+                moveDown(step);
             }
         }
         
-        ans=rCount*cCount;
-        return ans;
+        return count(x,y);
+
     }
 }
