3572d3cc1d13cba3beedd12334b14bf387e56725
Kwon <power916@daum.net>
2023-12-06 11:27:07 +0900

    231206 wolfy916 algorithm & CS 17~19

---
+++ Algorithm/2023.12/wolfy916/코딩테스트 기출문제/아날로그 시계.py
@@ -0,0 +1,11 @@
+'''
+아날로그 시계 - PCCP 기출문제
+분류 : 그리디
+'''
+
+def solution(h1, m1, s1, h2, m2, s2):
+    answer = -1
+    return answer
+
+if __name__ == '__main__':
+    print(solution(h1, m1, s1, h2, m2, s2))
