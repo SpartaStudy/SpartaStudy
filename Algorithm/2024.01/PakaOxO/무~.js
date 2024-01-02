37eed3f081ad730195c88fb81f41abee97d59512
PakaOxO <arpeggiogio@gmail.com>
2024-01-02 11:03:06 +0900

    merge: wolfy916

--- Algorithm/2024.01/PakaOxO/무~.js
+++ Algorithm/2024.01/PakaOxO/무~.js
@@ -10,8 +10,9 @@ const solution = () => {
   let answer = "";
 
   /* 메인 로직 */
+  // 각 스테이지별 길이 계산
   for (let i = 1; i <= N; i++) {
-    len[i] = len[i - 1] * 2 + i + 3;
+    len[i] = len[i - 1] * 2 + i + 3; // 이전 스테이지 앞 뒤 + 중간에 moo(o의 길이는 level + 2)
     if (len[i] >= N) {
       t = i;
       break;
@@ -24,15 +25,19 @@ const solution = () => {
 
   // find
   function find(level, pos) {
+    // 0단계까지 왔으면 moo에서 리턴
     if (level === 0) {
       return pos === 0 ? "m" : "o";
     }
 
     if (pos < len[level - 1]) {
+      // (이전 레벨**) + (moo...) + (이전 레벨)
       return find(level - 1, pos);
     } else if (pos < len[level - 1] + level + 3) {
+      // (이전 레벨) + (moo...**) + (이전 레벨)
       return pos - len[level - 1] === 0 ? "m" : "o";
     } else {
+      // (이전 레벨) + (moo...) + (이전 레벨**)
       return find(level - 1, pos - (len[level - 1] + level + 3));
     }
   }
